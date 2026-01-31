dec02979a95ceeda94b73bdc9894ae8be7deb47d
shreyash3128 <shreyashgurav317@gmail.com>
2026-01-31 16:55:25 +0530

    Day03: Sum of digits using recursion

--- day03_sumOfDigits.py
+++ day03_sumOfDigits.py
@@ -1,7 +1,8 @@
+def sumOfDigits (num, sum=0):
+    if num <= 0:
+        return sum
+    a = num % 10 
+    sum += a
+    return sumOfDigits(num//10, sum)
 num = int(input("enter a number : "))
-sum = 0
-while num > 0:
-    x = num % 10
-    sum += x
-    num //= 10
-print (f"Sum of the digits of entered number is : {sum}")
+print (f"Sum of the digits of entered number is : {sumOfDigits(num)}")
