34d502e85e090f40d6792d84f3eaa0f38810e260
shreyash3128 <shreyashgurav317@gmail.com>
2026-02-17 23:41:22 +0530

    Day12: smallest element in array using recursion

--- day12_max_element_in_array.py
+++ day12_max_element_in_array.py
@@ -8,3 +8,22 @@ while i < len(arr):
     i += 1
 
 print("Smallest element is:", min_element)
+
+# Recursive function to find smallest element
+
+def find_min(arr, n):
+    # Base condition
+    if n == 1:
+        return arr[0]
+    
+    min_of_rest = find_min(arr, n-1)
+    
+    if arr[n-1] < min_of_rest:
+        return arr[n-1]
+    else:
+        return min_of_rest
+
+
+arr = [10, 45, 23, 89, 12, 67]
+
+print("Smallest element is:", find_min(arr, len(arr)))
